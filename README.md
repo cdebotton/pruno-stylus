dfd10cbba64b4061a8b9565d2536a3ed267ab574
Christian de Botton <cdebotton@gmail.com>
2015-01-29 23:52:39 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,26 @@
+# pruno-http
+
+A Stylus mix for pruno that includes font-awesome, normalize.css, nib, jeet, and rupture.
+
+## Usage
+
+```js
+"use strict";
+
+var pruno = require('pruno');
+
+pruno.plugins(function(mix) {
+  mix
+    .configure({ dir: __dirname + '/config' })
+    .stylus({
+      'entry': '::src/stylus/index.styl',
+      'dist': '::dist/stylesheets/app.css',
+      'search': '::src/**/*.styl',
+      'minify': false,
+      'source-maps':true,
+      'font-awesome': false,
+      'normalize': false,
+      'use': ['nib', 'jeet', 'rupture']
+    });
+});
+```
---
+++ index.js
@@ -0,0 +1,105 @@
+"use strict";
+
+var pruno = module.parent.require('pruno');
+var path = require('path');
+var streamqueue = require('streamqueue');
+var plugins = require('gulp-load-plugins')();
+
+function StylusTask(params) {
+  this.params = (params || {});
+}
+
+StylusTask.getDefaults = function() {
+  return {
+    'entry': '::src/stylus/index.styl',
+    'dist': '::dist/stylesheets/app.css',
+    'search': '::src/**/*.styl',
+    'minify': false,
+    'source-maps':true,
+    'font-awesome': false,
+    'normalize': false,
+    'use': ['nib', 'jeet', 'rupture']
+  };
+};
+
+StylusTask.prototype.enqueue = function(gulp, params) {
+  params = (params || {});
+  var opts = distillOptions(StylusTask, params);
+
+  if (params['source-maps']) {
+    opts.sourcemap = {
+      inline: true,
+      sourceRoot: '.'
+    }
+  }
+
+  if (params.use && getType(params.use) === 'array') {
+    opts.use = params.use.map(m => require(m)());
+  }
+
+  return compileCSS({
+    gulp: gulp,
+    compiler: 'stylus',
+    opts: opts,
+    params: params
+  });
+};
+
+StylusTask.prototype.generateWatcher = function() {
+  return true;
+};
+
+function compileCSS(args) {
+  var gulp = args.gulp;
+  var compiler = args.compiler;
+  var opts = args.opts;
+  var params = args.params;
+  var parts = params.dist.match(/^(.+\/)(.+\.css)$/);
+  var dist = parts[0];
+  var distDir = parts[1];
+  var fileName = parts[2];
+  var stream = streamqueue({objectMode: true});
+
+  stream.pipe(
+    plugins.if(
+      params['source-maps'],
+      plugins.sourcemaps.init({loadMaps: true})
+    )
+  );
+
+  if (params.normalize) {
+    stream.queue(
+      gulp.src(pkg('normalize.css', 'normalize.css'))
+    );
+  }
+
+  if (params['font-awesome']) {
+    stream.queue(
+      gulp.src(pkg('font-awesome', 'css/font-awesome.css'))
+    );
+  }
+
+  stream.queue(
+    gulp.src(params.entry)
+      .pipe(plugins.plumber())
+      .pipe(plugins[compiler](opts))
+  );
+
+  return stream.done()
+    .pipe(plugins.concat(fileName))
+    .pipe(plugins.if(params.minify, plugins.minifyCss()))
+    .pipe(plugins.if(params['source-maps'], plugins.sourcemaps.write()))
+    .pipe(gulp.dest(distDir));
+}
+
+function pkg(pkgName, path) {
+  return './node_modules/pruno/node_modules/' + pkgName + '/' + path;
+};
+
+function getType(obj) {
+  return Object.prototype.toString.call(obj)
+    .match(/^\[object (.+)\]$/i)[1]
+    .toLowerCase();
+}
+
+module.exports = pruno.extend(StylusTask);
